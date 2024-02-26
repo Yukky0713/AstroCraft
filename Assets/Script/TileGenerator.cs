31ccd86297573ab0d27b2e4d2b3bbbb8d433fb47
Yukky0713 <161120036+Yukky0713@users.noreply.github.com>
2024-02-26 19:59:34 +0900

    作成：TileGeneratorの作成
    マップの生成用に、タイルを指定しただけ縦横に配置するスクリプトを作成しました。

--- Assets/Script/TileGenerator.cs
+++ Assets/Script/TileGenerator.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// オブジェクトを縦横に配置するスクリプト
+/// </summary>
 public class TileGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //クローンされるオブジェクト
+    [SerializeField] private GameObject objectPrefab;
+    //オブジェクト数
+    [SerializeField] private int rowCount = 10;
+    [SerializeField] private int columnCount = 10;
+    //オブジェクト間のスペース
+    [SerializeField] private float spacing = 1.0f;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //オブジェクトを縦横に配置
+        for(int row = 0; row < rowCount; row++)
+        {
+            for(int col =0; col < columnCount; col++)
+            {
+                Vector3 position = new Vector3(col * (10 + spacing),row * (10 + spacing),0);
+                Instantiate(objectPrefab,position,Quaternion.identity);
+            }
+        }
     }
 }
