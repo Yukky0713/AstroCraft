7b246a3f793c014530531366577ca3919b39922d
Yukky0713 <161120036+Yukky0713@users.noreply.github.com>
2024-02-27 20:17:49 +0900

    変更：タイルが中央に生成されるように
    今まで、(0,0)をスタートして生成されていたので世界がカメラの真ん中に来るようにした

--- Assets/Script/TileGenerator.cs
+++ Assets/Script/TileGenerator.cs
@@ -13,14 +13,21 @@ public class TileGenerator : MonoBehaviour
     [SerializeField] private int rowCount = 10;
     [SerializeField] private int columnCount = 10;
 
+    private float center_x;
+    private float center_y;
+
     private void Start()
     {
+        //タイル生成の開始地点
+        center_x = -(float)rowCount / 2.0f + 0.5f;
+        center_y = -(float)columnCount / 2.0f + 0.5f;
+
         //オブジェクトを縦横に配置
         for(int row = 0; row < rowCount; row++)
         {
             for(int col =0; col < columnCount; col++)
             {
-                Vector3 position = new Vector3(col,row,0);
+                Vector3 position = new Vector3(center_x + (float)col, center_y + (float)row, 0);
                 GameObject tile = Instantiate(objectPrefab,position,Quaternion.identity);
                 tile.transform.parent = transform;
             }
